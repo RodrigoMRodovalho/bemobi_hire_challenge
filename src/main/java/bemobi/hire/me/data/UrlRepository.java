702fa8b79551d492318f6ce1873c33c4b83efbda
rodrigomrodovalho <rodrigomrodovalho@gmail.com>
2017-02-08 00:16:14 -0200

    - Creates the service stub methods and repository methods declarations

--- src/main/java/bemobi/hire/me/data/UrlRepository.java
+++ src/main/java/bemobi/hire/me/data/UrlRepository.java
@@ -1,7 +1,19 @@
 package bemobi.hire.me.data;
 
+import bemobi.hire.me.domain.Url;
+
+import java.util.List;
+
 /**
  * Created by rrodovalho on 07/02/17.
  */
 public interface UrlRepository {
+
+    Url saveUrl(Url url);
+
+    Url getUrlByAlias(String alias);
+
+    int updateUrlAccess(int currentAccess, String alias);
+
+    List<Url> getMostFiveAccessedUrl();
 }
--- src/main/java/bemobi/hire/me/service/UrlService.java
+++ src/main/java/bemobi/hire/me/service/UrlService.java
@@ -1,10 +1,30 @@
 package bemobi.hire.me.service;
 
+import bemobi.hire.me.domain.Url;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by rrodovalho on 07/02/17.
  */
 @Service
 public class UrlService {
+
+
+    public Url reduceUrl(Url url){
+
+        return null;
+    }
+
+    public Url getExpandedUrl(String alias){
+
+        return null;
+    }
+
+    public List<Url> getMostAccessedUrls(){
+
+        return null;
+    }
+
 }
